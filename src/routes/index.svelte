1a7923016fe3e56c0ff2f5538df3f8a0e574cd0e
Simeon Tim Jackman <simja114@student.liu.se>
2020-11-01 12:31:06 +0100

    ADD: loading of players

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,32 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+import { onMount } from "svelte";
+let players = [];
+onMount(async () => {
+	console.log("Getting players")
+    await fetch(`http://premierleaguefantasy.site/players`,{
+		mode: 'cors',
+	})
+	.then(response => response.json())
+	.then(
+		(data) => {
+			players = data
+			console.log(players[0].first_name)
+		})
+  })
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+	
 </style>
 
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Players</h1>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	{#each players as player}
+		<li>
+			{player.first_name} {player.second_name} 
+		</li>
+	{/each}
