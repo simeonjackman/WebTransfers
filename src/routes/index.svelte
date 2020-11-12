fbc0edf237a4c3ee370ded5720314ff765602c63
Simeon Tim Jackman <simja114@student.liu.se>
2020-11-12 21:31:24 +0100

    ADD: filter and sort on the frontend

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,37 @@
 <script>
 import { onMount } from "svelte";
+let searchFilter = "";
 let players = [];
+$: currentPlayers = players.filter(item => item.first_name.toLowerCase().includes(searchFilter) || item.second_name.toLowerCase().includes(searchFilter.toLowerCase()));
+
 onMount(async () => {
 	console.log("Getting players")
-    await fetch(`http://premierleaguefantasy.site/players`,{
+    await fetch(`http://premierleaguefantasy.site/players?sort_by=transfers_in_event`,{
 		mode: 'cors',
 	})
 	.then(response => response.json())
 	.then(
 		(data) => {
 			players = data
+			currentPlayers = players
 			console.log(players[10])
 		})
   })
+  let lastKey = "";
+function sortPlayers(key) {
+		currentPlayers = currentPlayers.sort(function(a, b) {
+		// direction is used for reverse sort
+		let direction = (key == lastKey) ? 1 : -1
+		let keyA = a[key];
+		let keyB = b[key];
+		// Compare the 2 keys
+		if (keyA < keyB) return -1 * direction;
+		if (keyA > keyB) return 1 * direction;
+		return 0;
+		}).reverse();
+		lastKey = key;
+	}
+
 </script>
 
 <style>
@@ -24,33 +43,42 @@ onMount(async () => {
 </svelte:head>
 
 
+
+
+
 <div class="flex flex-col">
+
+	<input bind:value={searchFilter} class="px-6 py-3 bg-gray-50 text-left font-medium text-blue-500 uppercase tracking-wider rounded-full" type="text" placeholder="Search...">
+
 	<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 		<div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
 			<div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
 				<table class="min-w-full divide-y divide-gray-200">
 				<thead>
 					<tr>
-					<th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+					<th on:click={() => sortPlayers("second_name")} class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
 						Name
 					</th>
-					<th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+					<th on:click={() => sortPlayers("now_cost")} class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
 						Cost
 					</th>
-					<th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+					<th on:click={() => sortPlayers("transfers_in_event")} class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
 						Transfers
 					</th>
-					<th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+					<th on:click={() => sortPlayers("goals_scored")} class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
 						Goals
 					</th>
-					<th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+					<th on:click={() => sortPlayers("assists")} class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
 						Assists
+					</th>
+					<th on:click={() => sortPlayers("total_points")} class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+						Total Points
 					</th>
 					<th class="px-6 py-3 bg-gray-50"></th>
 					</tr>
 				</thead>
 					<tbody class="bg-white divide-y divide-gray-200">
-						{#each players as player}
+						{#each currentPlayers.reverse() as player}
 							<tr>
 							<td class="px-6 py-4 whitespace-no-wrap">
 								<div class="flex items-center">
@@ -84,6 +112,11 @@ onMount(async () => {
 							<td class="px-6 py-4 whitespace-no-wrap">
 								<span class="px-2 inline-flex leading-5 rounded-full text-gray-900">
 								{player.assists}
+								</span>
+							</td>
+							<td class="px-6 py-4 whitespace-no-wrap">
+								<span class="px-2 inline-flex leading-5 rounded-full text-gray-900">
+								{player.total_points}
 								</span>
 							</td>
 							<td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
